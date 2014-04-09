be6635fc74fd1e900c1cdae8e9a066a84572ea7c
Vlasov Artem [Lenovo] <root@proscript.ru>
2014-04-09 17:30:02 +0400

    add new templatetag

--- v3d_libs/templatetags/v3d_tags.py
+++ v3d_libs/templatetags/v3d_tags.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals, absolute_import
 
 from django.template import Library
+from django.core.urlresolvers import resolve, reverse
+from django.utils.translation import activate, get_language
 
 register = Library()
 
@@ -12,3 +14,18 @@ def get_range0(value):
 @register.filter
 def get_range1(value):
     return range(1, value + 1)
+
+
+@register.simple_tag(takes_context=True)
+def change_lang(context, lang=None, *args, **kwargs):
+
+    url = context['request'].path
+    url_parts = resolve(path=url)
+    current_lang = get_language()
+    try:
+        activate(lang)
+        url = reverse(url_parts.view_name, kwargs=url_parts.kwargs)
+    finally:
+        activate(current_lang)
+
+    return '{0}'.format(url)
